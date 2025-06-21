b773167b6a35986fd27a0df8a16e3daed6e4e3c3
Bryan Johns <73319490+johbry17@users.noreply.github.com>
2025-06-20 22:17:47 -0400

    docs: update README, test year hook

--- README.md
+++ README.md
@@ -4,10 +4,14 @@ Collects various data science projects into one handy website.
 
 Use `./templates/00_post_template_sample.html` when adding a new project.
 
+Business card QR code located in `./resources/images/portfolio.png`.
+
+![QR Code](./resources/images/portfolio.png)
+
 ## Acknowledgements
 
 Thanks to Start Bootstrap for the [Clean Blog](https://startbootstrap.com/theme/clean-blog) Theme.
 
 ## Author 
 
-Copyright &copy; Bryan C. Johns, 2024
+Copyright &copy; Bryan C. Johns, <!-- START_DATE -->2025<!-- END_DATE -->
---
+++ resources/dynamic_readme_date.py
@@ -0,0 +1,39 @@
+'''
+This script updates the README.md file with the current month and year.
+It's automatically run every time a commit is made to the repository.
+Thank you, Git Hook!
+'''
+
+from datetime import datetime
+
+# filepath to README
+readme_path = "README.md"
+
+# get current month and year
+current_date = datetime.now()
+year = current_date.strftime("%Y")
+
+# where date will be inserted
+marker_start = "<!-- START_DATE -->"
+marker_end = "<!-- END_DATE -->"
+
+# read README
+with open(readme_path, "r") as file:
+    content = file.read()
+
+# insert current month and year
+if marker_start in content and marker_end in content:
+    updated_content = (
+        content.split(marker_start)[0]
+        + f"{marker_start}{year}{marker_end}"
+        + content.split(marker_end)[1]
+    )
+else:
+    print("Markers not found in README.md. No changes made.")
+    exit()
+
+# write updated content back to README.md
+with open(readme_path, "w") as file:
+    file.write(updated_content)
+
+print(f"Updated README.md with the current year: {year}")
